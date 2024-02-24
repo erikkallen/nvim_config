d8ea96419be1f7425681aa9c28b7fce92383e6c1
Erik Kallen <info@erikkallen.nl>
2024-02-24 22:01:53 +0100

    Add copilot, and tweak clangd to remove error

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,64 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    --
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            panel = {
+                enabled = true,
+                auto_refresh = false,
+                keymap = {
+                    jump_prev = "[[",
+                    jump_next = "]]",
+                    accept = "<CR>",
+                    refresh = "gr",
+                    open = "<M-CR>"
+                },
+                layout = {
+                    position = "bottom", -- | top | left | right
+                    ratio = 0.4
+                }
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = "<M-l>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>"
+                }
+            },
+            filetypes = {
+                yaml = false,
+                markdown = false,
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ["."] = false
+            },
+            copilot_node_command = 'node', -- Node.js version must be > 18.x
+            server_opts_overrides = {}
+        })
+    end,
+    {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+    {"rebelot/kanagawa.nvim"},
+    {"savq/melange-nvim"}
 }
